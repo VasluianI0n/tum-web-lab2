4484c7818da8e70f884d80ca559ebff817be0dad
Ion Vasluian <79792299+VasluianI0n@users.noreply.github.com>
2024-03-21 21:16:26 +0200

    Added mascot

---
+++ mascot.js
@@ -0,0 +1,9 @@
+function showMascot() {
+    var mascot = document.getElementById('mascot');
+    if(mascot){
+        mascot.style.display = 'inline-block';
+    }
+    
+  }
+  
+  setTimeout(showMascot, 3000);
--- script.js
+++ script.js
@@ -1,7 +1,20 @@
 document.addEventListener('DOMContentLoaded', function() {
+
+  setTimeout(function() {
+    var mascot = document.getElementById('mascot');
+    mascot.style.display = 'inline-block';
+    console.log("Done"); 
+}, 3000); 
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+ 
+
   // Add event listener to the menu icon
   document.getElementById('menuIcon').addEventListener('click', function() {
       var menuItems = document.getElementById('menuItems');
       menuItems.classList.toggle('show'); // Toggle the 'show' class on menu items
   });
+  
 });
+
